9b6f3c9ac93fb9153bc2d90b5cbae63e0bb92f2d
s-asif <99680917+s-asif@users.noreply.github.com>
2022-02-26 19:58:17 +0530

    Alert user in landscape mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'periodicTable';
+
+  landScape = window.matchMedia("(orientation: landscape)");
+
+  constructor() {
+    this.landScape.addEventListener("change", () => {
+      if (this.landScape.matches) {
+        alert('Landscape mode not fully supported. Please switch back to Portrait mode.')
+      }
+    });
+  }
 }

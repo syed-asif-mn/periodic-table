b62ff64c0657e9d55698ba9b6571b628ea5abbf1
syed-asif-mn <99680917+syed-asif-mn@users.noreply.github.com>
2022-04-03 18:09:00 +0530

    Update elements.component.ts

--- src/app/elements/elements.component.ts
+++ src/app/elements/elements.component.ts
@@ -87,11 +87,10 @@ export class ElementsComponent {
 
   //Random gradient generator
   generateBgColor() {
-    var r = Math.round(Math.random() * 255);
-    var g = Math.round(Math.random() * 255);
-    var b = Math.round(Math.random() * 255);
-    var gradient = `linear-gradient(${Math.round(Math.random() * 360)}deg,rgba(${r},${g},${b},0.2),rgba(${g},${r},${b},0.2))`;
+    var r = Math.round(Math.random() * 75);
+    var g = Math.round(Math.random() * 75);
+    var b = Math.round(Math.random() * 75);
+    var gradient = `linear-gradient(${Math.round(Math.random() * 360)}deg,rgba(${r},${g},${b}),rgba(${g},${r},${b}))`;
     return gradient;
   }
 }
-
